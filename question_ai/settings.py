cdf2985c865522bb13c70691ed0e62a099b35fbf
andre <andrebeet1@gmail.com>
2025-05-10 18:23:25 +0200

    Update settings.py

--- question_ai/settings.py
+++ question_ai/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-question-ai-123456'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['question-ai-r7ys.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
